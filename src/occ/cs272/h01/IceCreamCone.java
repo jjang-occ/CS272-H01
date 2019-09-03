fdd646337701b19a893fbff1eda916a4bcd9a923
jjang12 <jjang12@O074112-JJangD.coast.cccd.adx>
2019-09-03 16:15:37 -0700

    Completed IceCreamCone class

--- src/occ/cs272/h01/IceCreamCone.java
+++ src/occ/cs272/h01/IceCreamCone.java
@@ -5,19 +5,30 @@
 package occ.cs272.h01;
 
 /**
- * @author <your OCC login name such as sgilbert>
- * @version <the date>
+ * @author <jjang12>
+ * @version <9/3/2019>
  * 
  */
 public class IceCreamCone
 {
     // TODO: You must fill these in correctly to get credit
-    public static final String STUDENT = "Put your login ID here";
+    public static final String STUDENT = "jjang12";
     public static final String ASSIGNMENT = "H01-B";
 
     // TODO: Complete this class
     
+    private double height;
+    private double radius;
     
+    public IceCreamCone() { height = 0.0; radius = 0.0; }
+    public IceCreamCone(double h, double r) { height = h; radius = r; }
     
-    
+    public double getSurfaceArea()
+    {
+    	return Math.PI * radius * Math.sqrt(height * height + radius * radius);
+    }
+    public double getVolume()
+    {
+    	return Math.PI * radius * radius * height / 3.0;
+    }
 }
